8f1fac749ef8db7008e08b48b3dc9e546df79cd5
Salama Ashoush <salamaashoush@gmail.com>
2025-06-09 23:09:56 +0200

    fix format task

--- packages/utils/src/fs.ts
+++ packages/utils/src/fs.ts
@@ -1,6 +1,5 @@
-import { mkdir, writeFile as _writeFile,access } from "node:fs/promises";
+import { mkdir, writeFile as _writeFile, access } from "node:fs/promises";
 import { dirname } from "pathe";
-
 
 export async function ensureDir(dirPath: string): Promise<void> {
   if (!(await access(dirPath).catch(() => false))) {
@@ -8,10 +7,7 @@ export async function ensureDir(dirPath: string): Promise<void> {
   }
 }
 
-export async function writeFile(
-  filePath: string,
-  content: string
-): Promise<void> {
+export async function writeFile(filePath: string, content: string): Promise<void> {
   await ensureDir(dirname(filePath));
   await _writeFile(filePath, content.trim());
 }
