8773c1d316bde852bb4cac56f8747e89a502e29e
Salama Ashoush <salamaashoush@gmail.com>
2025-06-10 01:16:14 +0200

    fix api file in the default template

--- packages/create-pact-toolbox-app/templates/default/src/api/api.ts
+++ packages/create-pact-toolbox-app/templates/default/src/api/api.ts
@@ -1,35 +1,28 @@
 import { getUuid } from "@pact-toolbox/client";
 
 // importing from pact files
-import { deleteTodo, editTodo, newTodo, readTodo, readTodos, toggleTodoStatus } from "../../pact/todos.pact";
+import * as todosContract from "~/pact/todos.pact";
 
 export async function getAllTodos() {
-  return readTodos().build().dirtyRead();
+  return todosContract.getTodos().build().dirtyRead();
 }
 
 export async function getTodoById(id: string) {
-  return readTodo(id).build().dirtyRead();
+  return todosContract.getTodo(id).build().dirtyRead();
 }
 
 export async function editTodoById(id: string, title: string) {
-  return editTodo(id, title).sign().submitAndListen();
+  return todosContract.updateTodo(id, title).sign().submitAndListen();
 }
 
 export async function toggleTodoStatusById(id: string) {
-  return toggleTodoStatus(id).sign().submitAndListen();
+  return todosContract.toggleTodo(id).sign().submitAndListen();
 }
 
 export async function deleteTodoById(id: string) {
-  return deleteTodo(id).sign().submitAndListen();
+  return todosContract.deleteTodo(id).sign().submitAndListen();
 }
 
 export async function createTodo(title: string, id: string = getUuid()) {
-  console.log("creating todo", id, title);
-  return newTodo(id, title)
-    .sign()
-    .submitAndListen()
-    .then((tx) => {
-      console.log("created todo", tx);
-      return tx;
-    });
+  return todosContract.createTodo(id, title).sign().submitAndListen();
 }
