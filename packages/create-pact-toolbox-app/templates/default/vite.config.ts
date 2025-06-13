ccbf5d90f0d9424587f64cd9930132afafd00026
Salama Ashoush <salamaashoush@gmail.com>
2025-06-13 09:38:22 +0200

    fix template tests

--- packages/create-pact-toolbox-app/templates/default/vite.config.ts
+++ packages/create-pact-toolbox-app/templates/default/vite.config.ts
@@ -7,6 +7,7 @@ import pactVitePlugin from "@pact-toolbox/unplugin/vite";
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), pactVitePlugin()],
   test: {
+    environment: "happy-dom",
     testTimeout: 1000000,
     hookTimeout: 1000000,
     setupFiles: ["vitest.setup.ts"],
